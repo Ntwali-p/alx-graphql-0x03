43bddf1c07e04a00c6fa7dd5d7697be406d5a912
Ntwali-p <ntwali.pacific@gmail.com>
2025-12-19 21:16:20 +0200

    changes

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,41 +1,10 @@
-import React, { ReactNode } from "react";
-
-interface State {
-  hasError: boolean;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
 
-interface ErrorBoundaryProps {
-  children: ReactNode;
-}
+export default MyApp;
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error: Error): State {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log({ error, errorInfo });
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          <button onClick={() => this.setState({ hasError: false })}>
-            Try again?
-          </button>
-        </div>
-      );
-    }
-
-    return this.props.children;
-  }
-}
-
-export default ErrorBoundary;
--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/apolloClient";
+import ErrorBoundary from '@/components/ErrorBoundary';
+import type { AppProps } from "next/app";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
